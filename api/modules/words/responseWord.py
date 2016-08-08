a85ed30a06c65128bf653e8ee0d65f1bfff7af60
Renato Benkendorf <renato-benkendorf@hotmail.com>
2016-08-08 10:10:54 -0300

    Dont use more i

--- api/modules/words/responseWord.py
+++ api/modules/words/responseWord.py
@@ -2,7 +2,6 @@ class Response:
     isValid = {}
 
     def validLetter(self, word, userLetter):
-        i = 0
         self.isValid['isValid'] = False
         self.isValid['quantity'] = 0
         if userLetter in word:
